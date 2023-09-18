7f941561d3cbf63beea2a4a2b2af8bf56b21b202
Your Name <sineshaday19@github.com>
2023-09-18 05:22:29 -0700

    0-simply_match_school.rb

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is one command line argument provided
-if ARGV.length != 1
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Define the regular expression
-regex = /School/
-
-# Get the input string from the command line argument
-input_string = ARGV[0]
-
-# Check if the input string matches the regular expression
-if regex.match(input_string)
-  puts input_string
-else
-  exit 1
-end
+puts ARGV[0].scan(/School/).join
