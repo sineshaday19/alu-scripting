227ba0667a6685ce820ce69c00cd113f5a26d2dd
Sine Shaday <s.sine@alustudent.com>
2024-06-27 19:45:06 +0000

    update

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-0-main
-"""
-import sys
+"""Return the number of subscribers of a given subreddit"""
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
+        return 0
--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python3
-"""
-Script that queries the reddit API
-returns a list containing the titles of all hot articles for a given subreddit
-"""
+"""fetches the title of all hot posts for a given subreddit recursively"""
 
-
-import json
 import requests
-import sys
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    if hot_list is None:
-        hot_list = []
+def recurse(subreddit, hot_list=[], after=""):
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
-    headers = {"User-agent": "myRedditScript/1.0"}
-    params = {'after': after} if after else {}
-
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            new_posts = data['data']['children']
-            hot_list.extend([post['data']['title'] for post in new_posts])
-
-            after = data['data']['after']
-            if after:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
-        else:
-            return None
-
-    else:
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    PARAMS = {"after": after, "limit": 100}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, params=PARAMS,
+                                allow_redirects=False)
+        after = RESPONSE.json().get("data").get("after")
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [hot_list.append(post.get('data').get('title')) for post in HOT_POSTS]
+        if after is not None:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    except Exception:
         return None
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
